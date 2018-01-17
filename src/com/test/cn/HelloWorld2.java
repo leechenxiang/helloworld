2d98952d27f95be68ea283942e618351cc68193c
李晨翔 <chenxiang.li@bangdao-tech.com>
2018-01-17 14:39:57 +0800

    分支测试

---
+++ src/com/test/cn/HelloWorld2.java
@@ -0,0 +1,14 @@
+package com.test.cn;
+
+public class HelloWorld2 {
+
+    public static void main(String[] args) {
+        // 修改
+        System.out.println("hello world~~李晨翔");
+        // 主分支
+
+        // lee 分支上的修改
+        // ccc
+        System.out.println(12345);
+    }
+}
